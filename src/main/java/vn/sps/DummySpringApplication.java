3dba1e99f4b4019d00dcf0527f3e2f9644dedaaf
Le Van Tuan <levantuan.itvn@gmail.com>
2022-04-26 15:10:34 +0700

    Review spring framework

--- src/main/java/vn/sps/DummySpringApplication.java
+++ src/main/java/vn/sps/DummySpringApplication.java
@@ -2,10 +2,11 @@ package vn.sps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DummySpringApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DummySpringApplication.class);
+    public static void main(String[] args) throws Exception {
+        ApplicationContext applicationContext = SpringApplication.run(DummySpringApplication.class);
     }
 }
--- src/main/java/vn/sps/configs/DummyProperties.java
+++ src/main/java/vn/sps/configs/DummyProperties.java
@@ -1,6 +1,19 @@
 package vn.sps.configs;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class DummyProperties {
+
+    @PostConstruct
+    private void postConstruct() {
+        System.out.println("Post construct dummy properties.");
+    }
+
+    @PreDestroy
+    private void preDestroy() {
+        System.out.println("Pre destroy dummy properties.");
+    }
 
     private String applicationName = "Dummy Application";
 
