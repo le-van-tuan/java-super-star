4164565332f4fc452c0695dffd16ad0e1f6c430e
Le Van Tuan <levantuan.itvn@gmail.com>
2022-05-05 17:39:58 +0700

    Java collections

--- src/main/java/vn/sps/collections/Application.java
+++ src/main/java/vn/sps/collections/Application.java
@@ -4,27 +4,45 @@ import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
-        Set<String> stringSet = new TreeSet<>();
-        stringSet.add("b");
-        stringSet.add("c");
-        stringSet.add("a");
-        System.out.println("TreeSet....");
-        stringSet.forEach(System.out::println);
+        String[] arrayStr = new String[2];
+        String testStr[] = new String[2];
 
-        System.out.println("LinkedList...");
-        List<String> stringList = new LinkedList<>();
+        int ints[] = new int[3];
+        int[] testInts = new int[3];
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("a");
         stringList.add("c");
         stringList.add("b");
         stringList.add("a");
-        stringList.sort(Comparator.naturalOrder());
+        System.out.println("ArrayList...");
+        stringList.set(0, "Robin");
         stringList.forEach(System.out::println);
 
-        Map<String, Integer> map = new TreeMap<>();
-        map.put("b", 5);
-        map.put("c", 8);
-        ((TreeMap<String, Integer>) map).descendingMap();
-        for(Map.Entry<String, Integer> entry : map.entrySet()){
-            System.out.println("key : " + entry.getKey());
-        }
+        Set<String> stringSet = new HashSet<>();
+        stringSet.add("b");
+        stringSet.add("c");
+        stringSet.add("a");
+        stringSet.add("c");
+        System.out.println("HashSet ....");
+        stringSet.forEach(System.out::println);
+
+        Queue<String> stringQueue = new PriorityQueue<>();
+        stringQueue.add("1");
+        stringQueue.add("2");
+        stringQueue.add("3");
+        System.out.println("PriorityQueue");
+        stringQueue.forEach(System.out::println);
+        stringQueue.remove();
+        stringQueue.add("5");
+        stringQueue.forEach(System.out::println);
+
+        ArrayDeque<Integer> integerQueue = new ArrayDeque<>();
+        integerQueue.add(1);
+        integerQueue.add(2);
+        System.out.println("ArrayDeque");
+        integerQueue.forEach(System.out::println);
+        integerQueue.addFirst(0);
+        integerQueue.forEach(System.out::println);
     }
 }
